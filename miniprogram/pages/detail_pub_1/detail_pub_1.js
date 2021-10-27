7538a07a019584e2b3615046da1b93713c7d02f6
Linneaus18 <1678137130@qq.com>
2021-10-27 16:59:01 +0800

    jyc branch init

---
+++ miniprogram/pages/detail_pub_1/detail_pub_1.js
@@ -0,0 +1,85 @@
+// pages/detail_pub_1/detail_pub_1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info_content: {},
+    intro: "",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    var data = {
+      info_content: {
+        day: '2021年9月10日',
+        time: '11:00 - 13:00',
+        place: '清华大学气膜馆',
+        type: '击剑',
+        level: '高手',
+        offer_place: '是',
+        offer_tool: '否',
+        other: '男生优先；本科生优先',
+      },
+      intro: "这是一个面向高手开展的击剑活动，欢迎大家来找我练剑。",
+    }
+
+    that.setData({
+      info_content: data.info_content,
+      intro: data.intro,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Programs\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-3gqc1uw844dba4f3 --n quickstartFunctions --r --project "D:\Codes\LetsBall" --report_first --report
