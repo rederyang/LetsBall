5c191daa2ce20dbeed5606758e55238b54298ec9
Rederxz <1148002188@qq.com>
2021-12-01 15:45:42 +0800

    Refine main-more page.

--- code/miniprogram/pages/main-more/main-more.js
+++ code/miniprogram/pages/main-more/main-more.js
@@ -2,7 +2,10 @@
 Page({
   data: {
     keyWordSent: false,
-    searchWord: ""
+    searchWord: "",
+    step: 6,  // 每次刷新拉取任务数
+    result: [],
+    latestTasks: [],
   },
 
   bindInputChange: function(e) {
@@ -12,6 +15,16 @@ Page({
       searchWord: e.detail.value
     })
     console.log(this.data.searchWord)
+  },
+
+  onTapDetail: function(event) {
+    var taskId = event.currentTarget.dataset.taskid
+    console.log("获取到任务id:" + String(taskId))
+
+    // 默认以报名者身份跳转
+    wx.navigateTo({
+      url: '../detail_sub/detail_sub?taskId=' + taskId,
+    })
   },
 
   onTapSearch: function(event) {
@@ -44,6 +57,22 @@ Page({
     // FAKE DATA
     that.setData({
       result: [
+        {
+          test: 12
+        },
+        {
+          test: 12
+        },
+        {
+          test: 12
+        },
+        {
+          test: 12
+        },
+        {},
+        {},
+        {},
+        {},
         {}
       ],
     }
@@ -74,20 +103,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("test")
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {},
+  onShow: function () {
+    var that = this
+    that._getLatestTask()
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -114,7 +144,13 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("已经到底了")
+    // 搜索状态下不分页拉取
+    if (this.data.keyWordSent) {
+      return
+    }
+    // 非搜索状态下
+    this._getLatestTask(this.data.numLatestTask + this.data.step)
   },
 
   _search: async function(keyWord) {
@@ -134,6 +170,59 @@ Page({
         }
         that.setData({
           // TODO
+        })
+      } else {
+        wx.showModal({
+          title: '抱歉，出错了呢~',
+          content: res.result.errMsg,
+          confirmText: "我知道了",
+          showCancel: false,
+          success(res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
+        })
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  // 获得最新任务
+  _getLatestTask: async function (numTask=10) {  // 初始状态下拉取10个
+    var that = this
+
+    // 调用云函数
+    try {
+      var res = await wx.cloud.callFunction({
+        name: 'get_latest_task',
+        data: {
+          num: numTask
+        },
+      })
+      if (res.result.errCode == 0) {  
+        let latestTasks = res.result.data.tasks
+        console.log(latestTasks)
+        if (latestTasks == undefined) {
+          latestTasks = []
+        }
+        latestTasks = latestTasks.map(
+          item => {
+            let startTime = new Date(item.startTime)
+            let date = startTime.getFullYear() + '年' + (startTime.getMonth() + 1) + '月' + startTime.getDate() + '日'
+            let time =  startTime.getHours() + '点' + startTime.getMinutes() + '分'
+            let strTime = date + time + '开始'
+            res = item
+            res.startTime = strTime
+            return res
+          }
+        )
+        that.setData({
+          latestTasks: latestTasks,
+          numLatestTask: latestTasks.length
         })
       } else {
         wx.showModal({
